0967c2dba399131bfb2f51ae18911eabdf302d10
Saleem Taraqji <saleemtaraqji68@gmail.com>
2023-11-06 18:05:48 +0200

    3_Token_user

--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers, status
 from .models import Meal, Rating
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
 
-
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'password')
+        extra_kwargs = {'password': {'write_only': True, 'required': True}}
+    
 
 
 class MealSerializer(serializers.ModelSerializer):
